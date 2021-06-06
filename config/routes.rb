3ac9a45c92ced93265f76d19e5c2a35cc938b8c1
nicobazzoni <jillianamagsila@Jillians-iMac.fios-router.home>
2021-06-06 09:49:34 -0400

    commit tree 8fb478d4e0053661e027d7f8cab2aa437e10318a

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles, only: [:index, :show, :new, :create]
+  resources :articles, only: [:index, :show, :edit, :new, :create, :update]
+  get 'articles/:article_id', to: 'article#edit'
 end
